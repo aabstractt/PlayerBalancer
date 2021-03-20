ffc797f1216b62c539662b0b40d8c3706cd2948d
Xavier <xavier00.martinez@gmail.com>
2021-03-19 22:25:34 -0600

    a?

--- src/main/java/com/ithetrollidk/playerbalancer/PlayerBalancer.java
+++ src/main/java/com/ithetrollidk/playerbalancer/PlayerBalancer.java
@@ -1,2 +1,17 @@
-package com.ithetrollidk.playerbalancer;public class PlayerBalancer {
+package com.ithetrollidk.playerbalancer;
+
+import dev.waterdog.plugin.Plugin;
+
+public class PlayerBalancer extends Plugin {
+
+    private static PlayerBalancer instance;
+
+    public static PlayerBalancer getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
 }
